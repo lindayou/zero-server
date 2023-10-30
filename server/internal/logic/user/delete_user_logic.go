b1282a393176b17a8aa743307274c390e13899c0
linl <837706731@qq.com>
2023-10-30 17:42:18 +0800

    11111

---
+++ server/internal/logic/user/delete_user_logic.go
@@ -0,0 +1,35 @@
+package user
+
+import (
+	"context"
+
+	"zero-server/server/internal/svc"
+	"zero-server/server/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type DeleteUserLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewDeleteUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DeleteUserLogic {
+	return &DeleteUserLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *DeleteUserLogic) DeleteUser(req *types.DeleteUserReq) (resp *types.DeleteUserResp, err error) {
+	// todo: add your logic here and delete this line
+	resp = new(types.DeleteUserResp)
+	err = l.svcCtx.UserModel.DeleteUser(l.ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+	resp.Message = "删除成功"
+	return resp, nil
+}
